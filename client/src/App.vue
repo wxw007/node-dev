244c1d94a60cc2021bb8a45f3248bd7043526d34
wxw007 <359443034@qq.com>
2019-08-09 21:47:56 +0800

    dev

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,4 +8,23 @@
 
 <style lang="less">
 
+
+/* fade-transform */
+// .fade-transform-leave-active,
+.fade-transform-enter-active {
+  transition: all .3s;
+}
+
+.fade-transform-enter {
+  // opacity: .8;
+  transform: translateX(-30px);
+  z-index: 1;
+}
+
+// .fade-transform-leave-to {
+//   opacity: .6;
+//   transform: translateX(30px);
+// }
+
+
 </style>
